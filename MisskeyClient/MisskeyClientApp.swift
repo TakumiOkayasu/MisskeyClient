2a76c128657d00ef718a60df3e6c6cb9f26b5185
ウヰスキー響 <yamazaki@uwisukikyounoMacBook-Air.local>
2023-07-02 19:49:28 +0900

    Initial Commit

---
+++ MisskeyClient/MisskeyClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisskeyClientApp.swift
+//  MisskeyClient
+//
+//  Created by ウヰスキー響 on 2023/07/02.
+//
+
+import SwiftUI
+
+@main
+struct MisskeyClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
